5fab73fa2f31c8efb1e0d4bf55b43b5ad091c983
JuanMartinN02 <juannogueiram@gmail.com>
2026-07-30 11:25:17 -0400

    Order Class changes and Trade Class implementation

---
+++ src/main/java/com/juanmartin/orderbook/engine/OrderBook.java
@@ -0,0 +1,47 @@
+package com.juanmartin.orderbook.engine;
+
+import com.juanmartin.orderbook.domain.Order;
+import com.juanmartin.orderbook.domain.OrderType;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+// Container that stores and organizes the orders that haven't been completed
+public class OrderBook {
+    // ConcurrentSkipListMap as it automatically keeps keys sorted
+    /* ConcurrentLinkedQueue ensures that When multiple orders come
+    in at the exact same price , the queue ensures the order that arrived
+    first gets executed first FIFO*/
+    // Key is the Price of the order and the Queue is for the orders in FIFO
+    ConcurrentSkipListMap<BigDecimal, ConcurrentLinkedQueue<Order>> bids = new ConcurrentSkipListMap<>();
+    ConcurrentSkipListMap<BigDecimal, ConcurrentLinkedQueue<Order>> asks = new ConcurrentSkipListMap<>(Comparator.reverseOrder());
+
+    public void addOrder(Order order){
+        // Check if order is ASK or BID
+        if (order.getOrderType() == OrderType.ASK){
+            // Check if price exists on the book Keys, if not creates a new Queue for that price.
+            if(asks.containsKey(order.getPrice())){
+                ConcurrentLinkedQueue<Order> orderList = asks.get(order.getPrice());
+                orderList.add(order);
+            }else {
+                ConcurrentLinkedQueue<Order> orderList = new ConcurrentLinkedQueue<>();
+                orderList.add(order);
+                asks.put(order.getPrice(), orderList);
+            }
+        }else {
+            // Check if price exists on the book Keys, if not creates a new Queue for that price.
+            if(bids.containsKey(order.getPrice())){
+                ConcurrentLinkedQueue<Order> orderList = bids.get(order.getPrice());
+                orderList.add(order);
+            }else {
+                ConcurrentLinkedQueue<Order> orderList = new ConcurrentLinkedQueue<>();
+                orderList.add(order);
+                bids.put(order.getPrice(), orderList);
+            }
+        }
+    }
+
+
+}
